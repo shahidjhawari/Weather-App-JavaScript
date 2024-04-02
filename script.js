9819ad98eb51eae950ee57fddc18844c91de69a8
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-02 23:32:02 +0500

    fetch func

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const apiKey = "ec08f1281a3df94c94159431aa1a6cf3";
 const city = "murree";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function fetchWeather() {
+    fetch(apiUrl)
+      .then((response) => {
+        return response;
+      })
+  }
