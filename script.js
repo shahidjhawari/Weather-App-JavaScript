db3d48649116c209129848b5303c31513a5e6588
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-03 19:52:46 +0500

    design done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = "ec08f1281a3df94c94159431aa1a6cf3";
-const city = "murree";
+const city = "sargodha";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 function fetchWeather() {
@@ -8,7 +8,7 @@ function fetchWeather() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      console.log(data.main.temp);
     })
     .catch((error) => {
       console.error("Error fetching weather:", error);
